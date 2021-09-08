782aefea1e72a6335007c90b67e57b22aaa8b1b6
hasanfuad <hasanmdfuad@hotmail.com>
2021-09-08 19:20:41 +0600

    connect with mongodb burj al arab commit

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBrhpNH1ncrHrzBbfwM2tVmLQGbbBmx7SE",
+  authDomain: "burj-al-arab-uae-2021.firebaseapp.com",
+  projectId: "burj-al-arab-uae-2021",
+  storageBucket: "burj-al-arab-uae-2021.appspot.com",
+  messagingSenderId: "1057646656894",
+  appId: "1:1057646656894:web:1a97a8609ac902172c072f"
+};
+  export default firebaseConfig;
